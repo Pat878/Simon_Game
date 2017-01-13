dc4b9548137b24888801dc0310458044c3691718
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-01-12 19:59:58 -0500

    Updated js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
- var letsPlay, remove, playerMove;
-
+  /* var letsPlay, remove, playerMove; */
+ var turn = 0;
   var snd = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 
   /* $(".inner4").mouseover(function() {
@@ -16,27 +16,51 @@ $(document).ready(function(){
 
 
   function letsPlay(){
-  $(".inner4").addClass("glow")
+  var firstMove = $(".inner4").addClass("glow")
     snd.play();
     setTimeout(function () {
     $(".inner4").removeClass('glow');
 }, 500);
 
-}
+    if (turn == 1) {
+      setTimeout(function() {firstMove}, 500);
 
-  $(".start").click(function(){
-    letsPlay();
-    playerMove();
-  })
+      $(".inner3").addClass("glow")
+    snd.play();
+    setTimeout(function () {
+    $(".inner3").removeClass('glow');
+}, 500);
+    }
+
+}
 
   function playerMove(){
 
     var timer = setTimeout(function(){alert("Too slow!")}, 5000)
 
     var checkMove = document.getElementById("test").addEventListener("click", function() {
+      $(".inner4").addClass("glow")
+    snd.play();
+    setTimeout(function () {
+    $(".inner4").removeClass('glow');
+}, 500);
 clearTimeout(timer);
-      alert("You're doing great.") })
+      turn = 1
+      setTimeout(
+            function() {
+                letsPlay();
+            },
+            1000);
+
+        })
   }
+
+
+  $(".start").click(function(){
+    letsPlay();
+    playerMove();
+  })
+
 
 
 
